2ccddc26f5e458eecc60f26ad5e94f7717166063
Komplexnupe <leonard.love87@icloud.com>
2020-08-18 00:12:42 -0400

    update

--- assets/JS/weather.js
+++ assets/JS/weather.js
@@ -4,7 +4,7 @@ var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="
 var searchBtn = $(".search_icon");
 var currentDay = $("#jumboCD");
 var todaysDate = moment().format("dddd, MMMM Do YYYY");
-
+var forecast5 = "";
 var cities = ["Charlotte"];
 
 // Display Current Day on Jumbotron
@@ -29,46 +29,50 @@ function displayCityBtn() {
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
 
 
-        // displayForecast(city)
+        displayForecast(city)
     });
 }
 
-// // Display 5 Day Forecast
-// function displayForecast(city) {
-//     var queryURL = forecastURL + city + apiKey;
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response);
-//         var id = 1
-        
-//         for (let i = 0; i < response.list.length; i++) {
-//             var curr = response.list[i];
-//             // id = id++;
-            
-//             if (curr.dt_txt.includes("12:00")) {
-//                 console.log(id)
-//                 // console.log(curr.dt_txt);
-//                 // console.log([i]);
-//                 var iconURL = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png"
-//                 var weatherIcon = $("<img>");
-//                 weatherIcon.attr("src", iconURL);
-//                 weatherIcon.attr("alt", "weather icon");
-//                 var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-//                 var windSpeed = response.list[i].wind.speed
-//                 var humidity = response.list[i].main.humidity
-//                 var forecastDiv = $("div").appendTo("#forecast")
-//                 $("<div>").html("<h4>" + response.list[i].dt_txt + "</h4>").appendTo;
-                
-//                 //  $("#" + id).append(weatherIcon)
-//                 //   $("#temp" + id).text(tempF.toFixed(1) + " F");
-//                 //   $("#wind" + id).text("Wind Speed: " + windSpeed + " MPH");
-//                 //   $("#humidity" + id).text("Humidity: " + humidity + "%");
-//             }
-//         }
-//     });
-// }
+// Display 5 Day Forecast
+function displayForecast(city) {
+    var queryURL = forecastURL + city + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+       
+        for (let i = 0; i < response.list.length; i++) {
+            var curr = response.list[i];
+            if (curr.dt_txt.includes("12:00")) {
+                var newDiv = $("<div>").appendTo("#forecast");
+                newDiv.addClass("5day col-2 float-left ml-4 bg-light rounded");
+                // console.log(curr.dt_txt);
+                console.log(response.city.name)
+                console.log([i]);
+                var iconURL = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png"
+                var weatherIcon = $("<img>");
+                weatherIcon.attr("src", iconURL);
+                weatherIcon.attr("alt", "weather icon");
+                var temp = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+                var tempF = temp.toFixed(1)
+                var windSpeed = response.list[i].wind.speed
+                var humidity = response.list[i].main.humidity
+                var city = response.city.name
+                // $(".5day").attr("style", width, 200)
+                // $(".5day").attr("style", height, 200)
+                $(".5day").text(city + " " + tempF + " " + windSpeed + " " + humidity)
+                // "<br>" + tempF + "<br>" + windSpeed + "<br>" + humidity);
+                // $(".5day").append(weatherIcon);
+                // $(".5day").text(tempF);
+                // $(".5day").text(windSpeed);
+                // $(".5day").text(humidity);
+
+// class="col-2 float-left ml-4 bg-light rounded" style="height: 200px; width: 200px;"
+            }
+        }
+    });
+}
 
 // Create Button Divs for City History
 function renderDivs() {
